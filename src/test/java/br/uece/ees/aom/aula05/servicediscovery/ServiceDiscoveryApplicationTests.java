d0b1b87ed97004930afdff8baf1b23d45b4f0783
Anderson Severo <andersonsevero@gmail.com>
2024-04-25 00:08:09 -0300

    Commit inicial

---
+++ src/test/java/br/uece/ees/aom/aula05/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uece.ees.aom.aula05.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
